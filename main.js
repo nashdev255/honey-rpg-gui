6dd5eb224101b00210fc47b7b9f7fff5aa337492
nashdev255 <yutori1107@gmail.com>
2023-09-24 20:24:49 +0900

    set FRAME_RATE

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 'use strict'
 
+const FONT_SIZE = "48px monospace";
+const FRAME_RATE = 60;
+const PERIOD_MS = 1000 / FRAME_RATE;
+
 window.onload = () => {
   const canvas = document.getElementById('main');
   const display = canvas.getContext('2d');
-  display.fillText('Hello, World.', 0, 64);
+  display.font = FONT_SIZE;
+  display.fillText('Hello World', 0, 48);
+
+  setInterval(() => { WmTimer() }, PERIOD_MS);
 };
