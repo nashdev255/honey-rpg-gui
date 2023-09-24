9c7cbb8f96af5a28d92da0903d1b51015bb27047
nashdev255 <yutori1107@gmail.com>
2023-09-24 21:20:59 +0900

    add initializer

--- main.js
+++ main.js
@@ -10,21 +10,27 @@ const PERIOD_MS = 1000 / FRAME_RATE;
 /* 画像 */
 let mapImage;
 
+/* 描画 */
+let canvas;
+let display;
+
 /* フレーム数 */
 let displayFrame = 0;
 
-const WmTimer = () => {
-  const canvas = document.getElementById('main');
-  const display = canvas.getContext('2d');
+const initCanvas = () => {
+  canvas = document.getElementById('main');
 
   /* キャンバスの大きさを画面に合わせる */
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+}
 
+const initDisplay = () => {
+  display = canvas.getContext('2d');
   display.font = FONT_SIZE;
+}
 
-  displayFrame++;
-
+const WmPaint = () => {
   for ( let y = 0; y < 32 ; y++ ) {
     for ( let x = 0; x < 48; x++ ) {
       display.drawImage(mapImage, x*32, y*32);
@@ -34,9 +40,18 @@ const WmTimer = () => {
   display.fillText('Hello World' + displayFrame, 0, 48);
 }
 
+/* フレームごとに */
+const update = () => {
+  displayFrame++;
+  WmPaint();
+}
+
 window.onload = () => {
   mapImage = new Image();
   mapImage.src = './assets/map.png'
 
-  setInterval(() => { WmTimer() }, PERIOD_MS);
+  initCanvas();
+  initDisplay();
+
+  setInterval(() => update(), PERIOD_MS);
 };
