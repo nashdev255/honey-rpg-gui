ea788a944d83a8194832a2c22296f114470b8fa3
nashdev255 <yutori1107@gmail.com>
2023-09-24 19:00:29 +0900

    get canvas, set context

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 'use strict'
 
 window.onload = () => {
-  console.log('Hello, World.');
+  const canvas = document.getElementById('main');
+  const display = canvas.getContext('2d');
+  display.fillText('Hello, World.', 0, 64);
 };
